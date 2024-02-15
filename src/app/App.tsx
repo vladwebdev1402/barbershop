8eded20f4d09b2297d21eb539bffef3230406285
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-02-16 04:14:57 +0700

    init

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import { MainPage } from '@/pages/MainPage';
+
+import './fonts/fonts.scss';
+import './index.scss';
+
+function App() {
+  return <MainPage />;
+}
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite';
 import path from 'node:path';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
+
+import react from '@vitejs/plugin-react-swc';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
@@ -10,5 +12,12 @@ export default defineConfig({
   },
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, './src/') }],
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "/src/shared/scss/_variables.scss";',
+      },
+    },
   },
 });
